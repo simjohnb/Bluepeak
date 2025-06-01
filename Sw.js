4c5bfd87d85283e5ef858c0316381566b8ed6cea
simjohnb <simonejohnson840@gmail.com>
2025-05-31 20:05:16 -0500

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'jamaica-map-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css',
+  'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js',
+  'https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css',
+  'https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.min.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
